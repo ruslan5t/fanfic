7a96adef46b96b8a774fbbd41112835fb12a2813
ruslan5t <ruslan.v4@mail.ru>
2013-08-23 16:55:21 +0200

    some errors commit

--- src/main/java/by/itransition/fanfic/config/AppConfig.java
+++ src/main/java/by/itransition/fanfic/config/AppConfig.java
@@ -1,4 +1,4 @@
-package by.itransition.fanfic.cofnig;
+package by.itransition.fanfic.config;
 
 import java.util.Locale;
 
@@ -8,18 +8,14 @@ import javax.persistence.Persistence;
 import org.hibernate.search.jpa.Search;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
-import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.i18n.CookieLocaleResolver;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
-import org.springframework.web.servlet.i18n.SessionLocaleResolver;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @EnableWebMvc
