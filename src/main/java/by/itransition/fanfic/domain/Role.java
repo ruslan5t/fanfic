74acc069f10a12b0d6fc3f5aa0d4e713f8901d50
ruslan5t <ruslan.v4@mail.ru>
2013-08-25 13:21:41 +0200

    add static to constants

--- src/main/java/by/itransition/fanfic/domain/Role.java
+++ src/main/java/by/itransition/fanfic/domain/Role.java
@@ -8,11 +8,11 @@ import javax.persistence.Id;
 @Entity
 public class Role {
 	
-	public final String ROLE_USER = "ROLE_USER";
+	public static final String ROLE_USER = "ROLE_USER";
 	
-	public final String ROLE_ADMIN = "ROLE_ADMIN";
+	public static final String ROLE_ADMIN = "ROLE_ADMIN";
 	
-	public final String ROLE_ANONYMOUS = "ROLE_ANONYMOUS";
+	public static final String ROLE_ANONYMOUS = "ROLE_ANONYMOUS";
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
