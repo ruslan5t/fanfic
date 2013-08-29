21f235c49640914a9bb92f050f92615038777afb
ruslan5t <ruslan.v4@mail.ru>
2013-08-29 10:55:36 +0200

    Make roleService.

---
+++ src/main/java/by/itransition/fanfic/service/RoleService.java
@@ -0,0 +1,9 @@
+package by.itransition.fanfic.service;
+
+import by.itransition.fanfic.domain.Role;
+
+public interface RoleService {
+
+	Role getRoleByName(String name);
+	
+}
---
+++ src/main/java/by/itransition/fanfic/service/impl/RoleServiceImpl.java
@@ -0,0 +1,21 @@
+package by.itransition.fanfic.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import by.itransition.fanfic.dao.RoleDao;
+import by.itransition.fanfic.domain.Role;
+import by.itransition.fanfic.service.RoleService;
+
+@Service
+public class RoleServiceImpl implements RoleService {
+
+	@Autowired
+	private RoleDao roleDao;
+	
+	@Override
+	public Role getRoleByName(String name) {
+		return roleDao.getRoleByName(name);
+	}
+	
+}
